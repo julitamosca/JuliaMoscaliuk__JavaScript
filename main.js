6a13d634015a36589ff31dc885ef7aec67d9e33f
julitamosca <juliamoscaliuk@gmail.com>
2022-08-23 20:29:22 -0300

    Update main.js
    .

--- main.js
+++ main.js
@@ -5,22 +5,22 @@
 }
 */
 
-// let numeroFinalizacion = parseInt(prompt('Ingresa un numero'))
+let numeroFinalizacion = parseInt(prompt('Ingresa un numero'))
 
-// for(let i = 2; i<=numeroFinalizacion; i+2) {
-//     console.log('iteracion numero: '+i)
-// }
-
-
-function sumar(){
-    const numero1 = parseInt(prompt('ingresa un numero'))
-    const numero2 = parseInt(prompt('ingresa un nuevo numero'))
-
-    const resultado = numero1 + numero2
-    alert('el resultado es '+resultado)
+for (let i = 2; i <= numeroFinalizacion; i + 2) {
+    console.log('iteracion numero: ' + i)
 }
 
-sumar()
+
+// function sumar(){
+//     const numero1 = parseInt(prompt('ingresa un numero'))
+//     const numero2 = parseInt(prompt('ingresa un nuevo numero'))
+
+//     const resultado = numero1 + numero2
+//     alert('el resultado es '+resultado)
+// }
+
+// sumar()
 
 //while
 
