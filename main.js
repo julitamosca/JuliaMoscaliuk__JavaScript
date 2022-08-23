c6154a491fdbf20af602d1fe80d6af97e3f579eb
julitamosca <juliamoscaliuk@gmail.com>
2022-08-22 22:37:30 -0300

    prueba
    codigo siguiendo la clase 3

---
+++ main.js
@@ -0,0 +1,27 @@
+//for - conteo
+
+/*for(let i = 0; i<=10; i++) {
+    console.log('iteracion numero: '+i)
+}
+*/
+
+// let numeroFinalizacion = parseInt(prompt('Ingresa un numero'))
+
+// for(let i = 2; i<=numeroFinalizacion; i+2) {
+//     console.log('iteracion numero: '+i)
+// }
+
+
+function sumar(){
+    const numero1 = parseInt(prompt('ingresa un numero'))
+    const numero2 = parseInt(prompt('ingresa un nuevo numero'))
+
+    const resultado = numero1 + numero2
+    alert('el resultado es '+resultado)
+}
+
+sumar()
+
+//while
+
+//do...while
