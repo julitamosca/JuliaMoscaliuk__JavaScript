1a2cb1bc89594baa8c08ed9207844b549c7f4a37
julitamosca <juliamoscaliuk@gmail.com>
2022-08-24 20:29:20 -0300

    Update main.js
    Ejemplo de while y for

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-//for
+//Van ambos códigos en main. Comentar uno para ejecutar el otro 😊
 
-for(let i =1; i <= 10; i++){
-    alert(i);
-    console.log(alert); 
+//EJEMPLO WHILE
+
+let calificacion = prompt ("Ingresa la calificación obtenida");
+
+while(calificacion < 7) { 
+    alert("Calificación insuficiente. Debes repetir la prueba 😣");
+    calificacion= prompt("¿Repetiste la prueba? Ingresa tu calificación");
 }
+    alert("¡Felicitaciones! Prueba superada 😎");
+    
+
+//EJEMPLO FOR
+
+for (let i = 1; i <= 5; i++) {
+
+    let nombreUsuario = prompt("Ingresa tu nombre");
+
+    alert("Hola "+nombreUsuario+" Tienes el  N° "+i);
+}
+    
