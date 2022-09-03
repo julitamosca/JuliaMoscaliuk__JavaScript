3338cf04dae8883a0397040a6a15b12685568b99
julitamosca <juliamoscaliuk@gmail.com>
2022-09-03 13:27:38 -0300

    Práctica de arrays
    .

--- desafio3.js
+++ desafio3.js
@@ -1,4 +1,25 @@
 //ARRAYS
 
-const numeros = [1,2,3,4,5];
-console.log (numeros)
+/*const numeros = [1,2,3,4,5];
+console.log (numeros[0])
+console.log (numeros[3])
+
+let resultado = numeros[1] + numeros[2]
+console.log (resultado)*/
+
+/*const numeros = [1,2,3,4,5];
+for (let i=0; i<5; i++) {
+    alert(numeros[i]);
+}*/
+
+const miArray = ["marca", 3, "palabra"];
+miArray.push( 'otro elemento')
+console.log(miArray.length)
+console.log (miArray)
+
+
+/*const numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+for (let i = 0; i < numeros.length; i++) {
+    alert(numeros[i]);
+}
+console.log(numeros.length)*/
