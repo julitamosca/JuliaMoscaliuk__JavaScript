4a39c809332d8c19adae9441dc0064faa9b0bc8e
julitamosca <juliamoscaliuk@gmail.com>
2022-09-04 20:10:23 -0300

    Update desafio3.js
    Código de práctica en base a una guia

--- desafio3.js
+++ desafio3.js
@@ -1,4 +1,4 @@
-//ARRAYS
+//ARRAYS - practica 
 
 /*const numeros = [1,2,3,4,5];
 console.log (numeros[0])
@@ -19,12 +19,26 @@ miArray.unshift ('elemento al principio')
 // miArray.pop()
 console.log(miArray.length)
 
-miArray.splice(2,2)
+miArray.splice (2,2)
 console.log (miArray)
-console.log (miArray.join ("*"))
+console.log (miArray.join (" * "))
 
 /*const numeros = [1, 2, 3, 4, 5, 6, 7, 8]
 for (let i = 0; i < numeros.length; i++) {
     alert(numeros[i]);
 }
 console.log(numeros.length)*/
+
+const perros = ["Puppy", "Ronnie"]
+const gatos = ["Mishi", "Garfield", "Zuri"]
+const mascotas = perros.concat(gatos)
+console.log(mascotas)
+
+const nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+const masculinos = nombres.slice(1, 3);
+console.log(masculinos)
+console.log (nombres.indexOf('Rita'))
+console.log(nombres.includes('Julieta'))
+
+nombres.reverse()
+console.log(nombres)
