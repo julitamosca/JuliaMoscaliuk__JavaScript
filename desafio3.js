d0b6eed0088338fbb480f1053b652123c737b3cc
julitamosca <juliamoscaliuk@gmail.com>
2022-09-04 18:58:28 -0300

    Update desafio3.js
    .

--- desafio3.js
+++ desafio3.js
@@ -14,9 +14,14 @@ for (let i=0; i<5; i++) {
 
 const miArray = ["marca", 3, "palabra"];
 miArray.push( 'otro elemento')
+miArray.unshift ('elemento al principio')
+// miArray.shift()
+// miArray.pop()
 console.log(miArray.length)
-console.log (miArray)
 
+miArray.splice(2,2)
+console.log (miArray)
+console.log (miArray.join ("*"))
 
 /*const numeros = [1, 2, 3, 4, 5, 6, 7, 8]
 for (let i = 0; i < numeros.length; i++) {
